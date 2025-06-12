a6dfe02cacf810012e05eb22af14f64759ba4ea2
Vandeptry <emangafromnowhere@gmail.com>
2025-06-12 19:46:54 +0700

    init docker

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,12 +1,24 @@
+//src/app/page/home/home.component.ts
 import { Component } from '@angular/core';
+import { purchase } from '../../types/purchase';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  data: purchase[] = [];
+  ngOnInit(): void {
+    fetch('https://purchase-api.onrender.com/purchase')
+      .then((res) => res.json())
+      .then((result: purchase[]) => {
+        this.data = result;
+        console.log('Có dữ liệu trả về nè: ', this.data);
+      })
+      .catch((err) => console.error('Lỗi: ', err));
+  }
 }
---
+++ src/app/types/purchase.ts
@@ -0,0 +1,5 @@
+export interface purchase {
+  id: number;
+  created_at: Date;
+  desc: string;
+}
